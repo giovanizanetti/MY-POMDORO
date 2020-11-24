836af428efac8ed8a9828296220c86ac069dac9f
giovanizanetti <zanetti.giovani@gmail.com>
2020-11-24 15:35:56 +0100

    Add 'App Bar'.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css'
+import Navigation from './components/Navivation/index.js'
 
 function App() {
   return (
     <div className='App'>
+      <Navigation />
       <header className='App-header'>
         <p>My Pomodoro</p>
       </header>
