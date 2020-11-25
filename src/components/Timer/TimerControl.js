d1c7778634b3cd34efb6e76049d7d40c1e95cb7f
giovanizanetti <zanetti.giovani@gmail.com>
2020-11-25 01:30:19 +0100

    style controllers buttons

--- src/components/Timer/TimerControl.js
+++ src/components/Timer/TimerControl.js
@@ -1,17 +1,41 @@
 import React from 'react'
 import { Toolbar, Button } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
+import { PlayArrow, Stop, Restore, Pause } from '@material-ui/icons'
+
+const useTimerControlStyles = makeStyles((theme) => ({
+  root: {},
+  buttonSecondary: {
+    color: 'rgba(0,0,0,.87)',
+    backgroundColor: '#4caf50',
+    margin: theme.spacing(1),
+  },
+  controllers: {
+    margin: theme.spacing(1),
+  },
+}))
 
 const TimerControl = (props) => {
+  const { root, buttonSecondary, controllers } = useTimerControlStyles()
   return (
     <>
       <div>TimerControl</div>
-      <Toolbar variant='dense'>
-        <Button variant='contained'>Default</Button>
-        <Button variant='contained' color='primary'>
-          Primary
+      <Toolbar variant='dense' className={root}>
+        <Button variant='contained' className={buttonSecondary}>
+          <PlayArrow />
+          Start
         </Button>
-        <Button variant='contained' color='secondary'>
-          Secondary
+        <Button className={controllers} variant='contained' color='primary'>
+          <Pause />
+          Pause
+        </Button>
+        <Button className={controllers} variant='contained' color='secondary'>
+          <Stop />
+          Cancel
+        </Button>
+        <Button className={controllers} variant='contained' color='default'>
+          <Restore />
+          Reset
         </Button>
       </Toolbar>
     </>
