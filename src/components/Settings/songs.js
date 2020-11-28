862652da0df96ff1d61769333af2efbea366e900
giovanizanetti <zanetti.giovani@gmail.com>
2020-11-28 10:39:57 +0100

    feat: create song varibles list

---
+++ src/components/Settings/songs.js
@@ -0,0 +1,21 @@
+const alarmClock = '/songs/alarm_clock.mp3'
+const alarmNotLoud = '/songs/alarm_not_too_loud.mp3'
+const beatAlarm = '/songs/beat-alarm.mp3'
+const guitarAlarm = '/songs/guitar_alarm.mp3'
+const dickoryDockNotification = '/songs/hickory_dickory_dock-notification.mp3'
+const electro = '/songs/pruzhina_electro.mp3'
+const samsungS1 = '/songs/samsung_s1.mp3'
+const shorty = '/songs/shorty.mp3'
+const zvukBudilnika = '/songs/zvuk_budilnika.mp3'
+
+export const songList = [
+  alarmClock,
+  alarmNotLoud,
+  beatAlarm,
+  guitarAlarm,
+  dickoryDockNotification,
+  electro,
+  samsungS1,
+  shorty,
+  zvukBudilnika,
+]
