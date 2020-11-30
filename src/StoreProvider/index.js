568a1b4caf6adba0d3d827af7866641a69d13b1b
giovanizanetti <zanetti.giovani@gmail.com>
2020-11-30 17:32:15 +0100

    fix: short break initial state

--- src/StoreProvider/index.js
+++ src/StoreProvider/index.js
@@ -15,7 +15,7 @@ let initialState = {
   pomodoroDailyTarget: 0,
   pomodoroWeeklyTarget: 0,
   sendNotifications: true,
-  shortBreakLength: 5,
+  shortBreakLength: 300,
   openSettings: false,
   error: null,
   timerType: 'pomodoro',
