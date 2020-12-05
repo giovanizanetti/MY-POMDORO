33f846179d9440840d63bc5f69ddde4831308114
giovanizanetti <zanetti.giovani@gmail.com>
2020-12-05 13:46:13 +0100

    extract input for its own component

---
+++ src/components/Settings/SwitchBrowserNotifications.js
@@ -0,0 +1,22 @@
+import { FormControlLabel, Switch } from '@material-ui/core'
+import { Context } from '../../StoreProvider'
+import { SET_SEND_NOTIFICATIONS } from '../../types'
+import { useContext } from 'react'
+
+const SwitchDocTitleBrowser = () => {
+  const [state, dispatch] = useContext(Context)
+  return (
+    <FormControlLabel
+      control={
+        <Switch
+          checked={state.sendNotifications}
+          onChange={() => dispatch({ type: SET_SEND_NOTIFICATIONS })}
+          name='Browser notifications'
+        />
+      }
+      label='Browser notifications'
+    />
+  )
+}
+
+export default SwitchDocTitleBrowser
