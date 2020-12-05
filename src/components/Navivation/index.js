6a6fc6230bb1de5a64131b2375ed534d003c9b1c
giovanizanetti <zanetti.giovani@gmail.com>
2020-12-05 15:29:57 +0100

    style: show hamburguer menu for screens under 600px.

--- src/components/Navivation/index.js
+++ src/components/Navivation/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { AppBar, Toolbar, Typography, IconButton } from '@material-ui/core'
 import { useNavStyles } from './style'
 import MenuIcon from '@material-ui/icons/Menu'
@@ -7,8 +7,19 @@ import SideBar from './SideBar'
 const Navigation = () => {
   const { root, alignNavItems, logo, menuButton } = useNavStyles()
   const [isVisible, setIsVisible] = useState(false)
+  const [showHamburgerMenu, setShowHamburguerMenu] = useState()
+
   const toggleSideBar = () => setIsVisible(!isVisible)
 
+  useEffect(() => {
+    function handleResize() {
+      if (window.innerWidth < 600) setShowHamburguerMenu(true)
+      else setShowHamburguerMenu(false)
+      // console.log('resized to: ', window.innerWidth, 'x', window.innerHeight)
+    }
+
+    window.addEventListener('resize', handleResize)
+  })
   return (
     <>
       <AppBar className={root} position='static'>
@@ -16,15 +27,17 @@ const Navigation = () => {
           <Typography className={logo} variant='h6' color='inherit'>
             My Pomodoro
           </Typography>
-          <IconButton
-            edge='end'
-            className={menuButton}
-            color='inherit'
-            aria-label='menu'
-            onClick={() => toggleSideBar()}
-          >
-            <MenuIcon />
-          </IconButton>
+          {showHamburgerMenu && (
+            <IconButton
+              edge='end'
+              className={menuButton}
+              color='inherit'
+              aria-label='menu'
+              onClick={() => toggleSideBar()}
+            >
+              <MenuIcon />
+            </IconButton>
+          )}
         </Toolbar>
       </AppBar>
       <SideBar
