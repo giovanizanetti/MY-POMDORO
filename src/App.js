dcc499a4571c0b93c22a0356893cedf16682b727
giovanizanetti <zanetti.giovani@gmail.com>
2020-12-05 23:54:49 +0100

    style: remove timer background to show particles effect

--- src/App.js
+++ src/App.js
@@ -4,43 +4,15 @@ import Timer from './components/Timer'
 import StoreProvider from './StoreProvider/index'
 import Log from './components/Log'
 import Particles from 'react-particles-js'
+import { config } from './particles-config'
 
 function App() {
   return (
     <div className='App'>
       <StoreProvider>
         <Navigation />
-        <Particles
-          params={{
-            particles: {
-              line_linked: {
-                shadow: {
-                  enable: true,
-                  color: '#3CA9D1',
-                  blur: 5,
-                },
-              },
-              shape: {
-                polygon: {
-                  nb_sides: 20,
-                },
-              },
-            },
-            interactivity: {
-              events: {
-                onhover: {
-                  enable: true,
-                  mode: 'repulse',
-                },
-              },
-            },
-          }}
-          style={{
-            position: 'absolute',
-            width: '100%',
-            height: '100vh',
-          }}
-        />
+        {console.log(config)}
+        <Particles params={config} />
         <div className='App-header'>
           <Timer />
         </div>
