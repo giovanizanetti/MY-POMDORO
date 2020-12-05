df4a05e40a26f2d19abb7517b50cbce11d0471d2
giovanizanetti <zanetti.giovani@gmail.com>
2020-12-05 23:18:26 +0100

    feat: create interactive backgound

--- src/App.js
+++ src/App.js
@@ -3,15 +3,47 @@ import Navigation from './components/Navivation'
 import Timer from './components/Timer'
 import StoreProvider from './StoreProvider/index'
 import Log from './components/Log'
+import Particles from 'react-particles-js'
 
 function App() {
   return (
     <div className='App'>
       <StoreProvider>
         <Navigation />
-        <header className='App-header'>
+        <Particles
+          params={{
+            particles: {
+              line_linked: {
+                shadow: {
+                  enable: true,
+                  color: '#3CA9D1',
+                  blur: 5,
+                },
+              },
+              shape: {
+                polygon: {
+                  nb_sides: 20,
+                },
+              },
+            },
+            interactivity: {
+              events: {
+                onhover: {
+                  enable: true,
+                  mode: 'repulse',
+                },
+              },
+            },
+          }}
+          style={{
+            position: 'absolute',
+            width: '100%',
+            height: '100vh',
+          }}
+        />
+        <div className='App-header'>
           <Timer />
-        </header>
+        </div>
         <Log />
       </StoreProvider>
     </div>
