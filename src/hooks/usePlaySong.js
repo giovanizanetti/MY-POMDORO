700bee9678648d10095bff7e8c9066a2c59cba31
giovanizanetti <zanetti.giovani@gmail.com>
2020-12-07 15:59:25 +0100

    feat: play song otions on hoover

--- src/hooks/usePlaySong.js
+++ src/hooks/usePlaySong.js
@@ -1,16 +1,18 @@
 import { useEffect, useState } from 'react'
 
-export const usePlaySong = (alarmSong, audioRef) => {
+export const usePlaySong = ([alarmSong, audioRef]) => {
   const [isSongPlaying, setIsSongPlaying] = useState(false)
 
   useEffect(() => {
     console.log(alarmSong)
-    isSongPlaying && setIsSongPlaying(false) // clear cashed sound in initial render clear cashed sound
+    // isSongPlaying && setIsSongPlaying(false) // clear cashed sound in initial render clear cashed sound
 
     const stopSound = () => {
       audioRef.current.pause()
       audioRef.current.currentTime = 0
     }
+
+    if (!isSongPlaying) stopSound()
 
     if (isSongPlaying) {
       audioRef.current.play()
